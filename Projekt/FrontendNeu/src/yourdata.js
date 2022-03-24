333fa92bb23cfce6c8a3df3fdaead8163c0d7acc
raphaelschuster <91949221+raphaelschuster@users.noreply.github.com>
2022-03-24 10:41:11 +0100

    Add files via upload

--- Projekt/FrontendNeu/src/yourdata.js
+++ Projekt/FrontendNeu/src/yourdata.js
@@ -1,119 +1,163 @@
 export default {
 
-  //   Header Details ---------------------
-  headerTagline: [
-    //Line 1 For Header
-    "Building digital",
-    //Line 2 For Header
-    "products, brands,",
-    //Line 3 For Header
-    "and experience",
-  ],
   //   Header Paragraph
   headerParagraph:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
-
-  //Contact Email
-  contactEmail: "kontakt@yabe.com",
-
+    "Auf dieser Seite können Sie Artikel zum Verkauf anbieten sowie auf verschiedene, bereits erstellte Produkte bieten.",
+ 
   // End Header Details -----------------------
 
-  // Artikel Section ------------------------
-  projects: [
+  // Article Section ------------------------
+  articles: [
     {
-      title: "Project One", //Project Title - Add Your Project Title Here
+      title: "Fahrrad",
       para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
-      //Project Image - Add Your Project Image Here
+        "Neuwertig, 3 Jahre alt, neue Bremsen",
+      //image article
       imageSrc:
-        "https://images.unsplash.com/photo-1487837647815-bbc1f30cd0d2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Njl8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
-      //Project URL - Add Your Project Url Here
-      url: "http://chetanverma.com/",
+        "https://cdn.pixabay.com/photo/2013/07/13/13/43/racing-bicycle-161449_960_720.png",
+      //url article
+      url: "",
     },
     {
-      title: "Project Two", //Project Title - Add Your Project Title Here
+      title: "Mikrowelle",
       para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
-      //Project Image - Add Your Project Image Here
+        "von LG, Grau, 750 Watt",
+      //image article
       imageSrc:
-        "https://images.unsplash.com/photo-1605153864431-a2795a1b2f95?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwyNTY3ODl8fGVufDB8fHw%3D&auto=format&fit=crop&w=400&q=60",
-      //Project URL - Add Your Project Url Here
-      url: "http://chetanverma.com/",
+        "https://cdn.pixabay.com/photo/2012/04/11/17/29/microwave-29056__340.png",
+      //url article
+      url: "",
     },
     {
-      title: "Project Three", //Project Title - Add Your Project Title Here
+      title: "Artikel 3",
       para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
-      //Project Image - Add Your Project Image Here
+        "Artikeldetails",
+      //image article
       imageSrc:
         "https://images.unsplash.com/photo-1504083898675-c896ecdae86e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjJ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
-      //Project URL - Add Your Project Url Here
-      url: "http://chetanverma.com/",
+      //url article
+      url: "",
     },
     {
-      title: "Project Four", //Project Title - Add Your Project Title Here
+      title: "Artikel 4",
       para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
-      //Project Image - Add Your Project Image Here
+        "Artikeldetails",
+      //image article
       imageSrc:
         "https://images.unsplash.com/photo-1492171983775-a51717616c0d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjB8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
-      //Project URL - Add Your Project Url Here
-      url: "http://chetanverma.com/",
+      //url article
+      url: "",
     },
     {
-      title: "Project Five", //Project Title - Add Your Project Title Here
+      title: "Artikel 5",
       para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
-      //Project Image - Add Your Project Image Here
+        "Artikeldetails",
+      //image article
       imageSrc:
         "https://images.unsplash.com/photo-1534239143101-1b1c627395c5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NzZ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
-      //Project URL - Add Your Project Url Here
-      url: "http://chetanverma.com/",
+      //url article
+      url: "",
     },
     {
-      title: "Project Six", //Project Title - Add Your Project Title Here
+      title: "Artikel 6",
       para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
-      //Project Image - Add Your Project Image Here
+        "Artikeldetails",
+      //image article
       imageSrc:
         "https://images.unsplash.com/photo-1517479149777-5f3b1511d5ad?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTAyfHxwYXN0ZWx8ZW58MHx8MHw%3D&auto=format&fit=crop&w=400&q=60",
-      //Project URL - Add Your Project Url Here
-      url: "http://chetanverma.com/",
+      //url article
+      url: "",
     },
 
-    /*
-    If You Want To Add More Project just Copy and Paste This At The End
-    ,{
-        title: 'Project Five',
-        para: 'Something Amazing',
-        imageSrc: "",
-        url: ''
-    }
-    */
   ],
 
   // End Artikel Section -----------------------
 
+
+  // Offer Section ------------------------
+  offers: [
+  {
+    title: "Fahrrad",
+    para:
+      "Artikeldetails: Neuwertig, 3 Jahre alt, neue Bremsen",
+    //image article
+    imageSrc:
+      "https://images.unsplash.com/photo-1487837647815-bbc1f30cd0d2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Njl8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+    //url article
+    url: "",
+  },
+  {
+    title: "Artikel 2",
+    para:
+      "Artikeldetails",
+    //image article
+    imageSrc:
+      "https://images.unsplash.com/photo-1605153864431-a2795a1b2f95?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwyNTY3ODl8fGVufDB8fHw%3D&auto=format&fit=crop&w=400&q=60",
+    //url article
+    url: "",
+  },
+  {
+    title: "Artikel 3",
+    para:
+      "Artikeldetails",
+    //image article
+    imageSrc:
+      "https://images.unsplash.com/photo-1504083898675-c896ecdae86e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjJ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+    //url article
+    url: "",
+  },
+  {
+    title: "Artikel 4",
+    para:
+      "Artikeldetails",
+    //image article
+    imageSrc:
+      "https://images.unsplash.com/photo-1492171983775-a51717616c0d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjB8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+    //url article
+    url: "",
+  },
+  {
+    title: "Artikel 5",
+    para:
+      "Artikeldetails",
+    //image article
+    imageSrc:
+      "https://images.unsplash.com/photo-1534239143101-1b1c627395c5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NzZ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+    //url article
+    url: "",
+  },
+  {
+    title: "Artikel 6",
+    para:
+      "Artikeldetails",
+    //image article
+    imageSrc:
+      "https://images.unsplash.com/photo-1517479149777-5f3b1511d5ad?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTAyfHxwYXN0ZWx8ZW58MHx8MHw%3D&auto=format&fit=crop&w=400&q=60",
+    //url article
+    url: "",
+  },
+
+  ],
+
+  // End Offer Section -----------------------
+
+
   // About Section --------------
   aboutParaOne:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    "Wir sind ein junges Team, das sich darauf spezialisiert hat, Unternehmen bei der Digitalisierung ihrer Angebote zu unterstützen.",
   aboutParaTwo:
-    "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-  aboutParaThree:
-    "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    "Das Unternehmen Yabe stellt den Service bereit, als Benutzer auf dieser Website sowohl Artikel anlegen als auch auf Artikel bieten zu können.",
   aboutImage:
-    "",
+    "https://media.istockphoto.com/photos/weve-only-ever-achieved-great-things-together-picture-id1035530956?b=1&k=20&m=1035530956&s=170667a&w=0&h=kfFMN7XcBxuz903QOy25k5GXPDAvDJgxTZa3dDpE7-I=",
 
   //   End About Section ---------------------
 
+  // Kontakt Section --------------
 
-  //   Promotion Section --------------------------
+  contactEmail: "kontakt@yabe.com",
+  kontaktPara:
+  "© Web-Programmierung 2022.",
 
-  promotionHeading: "Heading",
-  promotionPara:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-  // End Promotion Section -----------------
-
+  // End Kontakt Section
+ 
 }
-
-// Thanks for using this template, I would love to hear from you contact me at hello@chetanverma.com
